31134ef2cc2b4c852d38128fc84c56a90c8c7d3b
sufianWG <webgurumrmbd@gmail.com>
2026-07-13 23:17:59 +0600

    first commit

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllPhotosPage = () => {
+    return (
+        <div>
+            All Photos
+        </div>
+    );
+};
+
+export default AllPhotosPage;
---
+++ src/components/TopGeneration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopGeneration = () => {
+    
+    return (
+        <div className='container mx-auto mt-5'>
+            <h1 className='text-lg font-bold'>Top Generations</h1>
+        </div>
+    );
+};
+
+export default TopGeneration;
